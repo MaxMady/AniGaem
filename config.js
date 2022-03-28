48040f660a69f97c3c4861743d59705c9ee862aa
MaxMady <81635616+MaxMady@users.noreply.github.com>
2022-03-28 20:58:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   token: "token here",
   servers: [""], //Server IDs enclosed in "" and seperated by ,
-  webhook: "https://discord.com/api/webhooks/957633844339691560/xkYq-jTBHzS2y3hvbjfOdiFNqy1C9HgVQeAeg8m8yVtdxyf6HABosR1bR2Uvc9Gv4YAn"//ID for webhook logger
+  webhook: "hook"//ID for webhook logger
 }
 
 module.exports = config
